e77c8ad044c5d1fc69a587120949a45d27f490b1
东风平夷 <98257102+Schuyn@users.noreply.github.com>
2026-02-11 14:33:43 -0500

    ver 1.2
    main exe之类的都是因为运行了mian代码的问题，其实只要在终端运行cl.exe /Zi /EHsc /nologo /Fo:build\ /Fe:bin\InputSchuyn.exe src\main.cpp生成InputSchuyn就完了，另外这次修复了找不到json的问题，因为之前cd了之后跑到项目根目录里了，应该就在bin里就地找。新增了ui提示，包括展示了每条rule，以及在成功切换输入法的时候在屏幕中心展示“Input Switched”

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: Chuyang Su cs4570@columbia.edu
  * @Date: 2026-02-11 13:47:46
- * @LastEditTime: 2026-02-11 14:19:54
+ * @LastEditTime: 2026-02-11 14:28:46
  * @FilePath: /InputSchuyn/src/main.cpp
  * @Description: 
  * 集成了窗口监控和自动切换输入法的逻辑
@@ -14,38 +14,105 @@
 #include <vector>
 
 #pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
 
 #define HKL_EN (HKL)0x04090409
 #define HKL_ZH (HKL)0x08040804
+#define TIMER_HIDE_UI 101
 
 // Global storage for rules
 std::map<std::wstring, HKL> appRules;
+HWND g_hwndUI = NULL;
+
+// --- Helper: Get EXE directory ---
+std::wstring GetExeDirectory() {
+    wchar_t buffer[MAX_PATH];
+    GetModuleFileNameW(NULL, buffer, MAX_PATH);
+    std::wstring path(buffer);
+    return path.substr(0, path.find_last_of(L"\\/"));
+}
+
+// --- UI: Simple Layered Window ---
+LRESULT CALLBACK UIWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    if (msg == WM_TIMER && wp == TIMER_HIDE_UI) {
+        ShowWindow(hwnd, SW_HIDE);
+        KillTimer(hwnd, TIMER_HIDE_UI);
+    }
+    if (msg == WM_PAINT) {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        // Draw a simple background
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        HBRUSH hBrush = CreateSolidBrush(RGB(45, 45, 45));
+        FillRect(hdc, &rect, hBrush);
+        DeleteObject(hBrush);
+        
+        // Draw Text (Language info)
+        SetTextColor(hdc, RGB(255, 255, 255));
+        SetBkMode(hdc, TRANSPARENT);
+        DrawTextW(hdc, L"Input Switched", -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        EndPaint(hwnd, &ps);
+    }
+    return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+void CreateIndicatorWindow() {
+    WNDCLASSW wc = { 0 };
+    wc.lpfnWndProc = UIWndProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = L"InputSchuynUI";
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    RegisterClassW(&wc);
+
+    g_hwndUI = CreateWindowExW(
+        WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_LAYERED | WS_EX_TRANSPARENT,
+        L"InputSchuynUI", L"", WS_POPUP,
+        0, 0, 150, 40, NULL, NULL, wc.hInstance, NULL
+    );
+    SetLayeredWindowAttributes(g_hwndUI, 0, 220, LWA_ALPHA); // Slight transparency
+}
+
+void ShowIndicator(HWND targetHwnd, bool isEn) {
+    if (!g_hwndUI) return;
+    
+    // Position: Near the center of the active window for now 
+    // (UIA caret positioning can be added later)
+    RECT rect;
+    GetWindowRect(targetHwnd, &rect);
+    int x = rect.left + (rect.right - rect.left) / 2 - 75;
+    int y = rect.top + (rect.bottom - rect.top) / 2 - 20;
+
+    SetWindowPos(g_hwndUI, HWND_TOPMOST, x, y, 0, 0, SWP_NOSIZE | SWP_NOACTIVATE);
+    ShowWindow(g_hwndUI, SW_SHOWNOACTIVATE);
+    InvalidateRect(g_hwndUI, NULL, TRUE); // Trigger repaint
+    SetTimer(g_hwndUI, TIMER_HIDE_UI, 1000, NULL); // Hide after 1s
+}
 
 // Simple JSON-like parser for the specific format you provided
 void LoadRulesJson() {
     appRules.clear();
-    std::wifstream file("rules.json");
+    std::wstring configPath = GetExeDirectory() + L"\\rules.json";
+    std::wifstream file(configPath);
+    
     if (!file.is_open()) {
-        std::wcerr << L"[InputSchuyn] Error: rules.json not found in bin folder!" << std::endl;
+        std::wcerr << L"[InputSchuyn] Error: rules.json NOT found at: " << configPath << std::endl;
         return;
     }
 
     std::wstring line;
     while (std::getline(file, line)) {
-        // Very basic parsing: find "Key": "Value"
-        size_t firstQuote = line.find(L'\"');
-        size_t secondQuote = line.find(L'\"', firstQuote + 1);
-        size_t thirdQuote = line.find(L'\"', secondQuote + 1);
-        size_t fourthQuote = line.find(L'\"', thirdQuote + 1);
-
-        if (firstQuote != std::wstring::npos && fourthQuote != std::wstring::npos) {
-            std::wstring exeName = line.substr(firstQuote + 1, secondQuote - firstQuote - 1);
-            std::wstring lang = line.substr(thirdQuote + 1, fourthQuote - thirdQuote - 1);
-            appRules[exeName] = (lang == L"EN" ? HKL_EN : HKL_ZH);
-            std::wcout << L"[InputSchuyn] Loaded Rule: " << exeName << L" -> " << lang << std::endl;
+        size_t f = line.find(L'\"');
+        size_t s = line.find(L'\"', f + 1);
+        size_t t = line.find(L'\"', s + 1);
+        size_t l = line.find(L'\"', t + 1);
+        if (f != std::wstring::npos && l != std::wstring::npos) {
+            std::wstring exe = line.substr(f + 1, s - f - 1);
+            std::wstring lang = line.substr(t + 1, l - t - 1);
+            appRules[exe] = (lang == L"EN" ? HKL_EN : HKL_ZH);
+            std::wcout << L"[InputSchuyn] Rule: " << exe << L" -> " << lang << std::endl;
         }
     }
-    file.close();
 }
 
 std::wstring GetProcessName(HWND hwnd) {
@@ -72,30 +139,19 @@ void CALLBACK WinEventProc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd,
         if (appRules.count(exeName)) {
             HKL target = appRules[exeName];
             PostMessage(hwnd, WM_INPUTLANGCHANGEREQUEST, 0, (LPARAM)target);
-            // Optional: console feedback
-            // std::wcout << L"[InputSchuyn] Switched " << exeName << std::endl;
+            ShowIndicator(hwnd, target == HKL_EN);
         }
     }
 }
 
 int main() {
-    std::wcout << L"--- InputSchuyn v1.1 Initializing ---" << std::endl;
-    
-    // 1. Load your rules.json
+    CreateIndicatorWindow();
     LoadRulesJson();
 
-    // 2. Set the Hook
     HWINEVENTHOOK hook = SetWinEventHook(
         EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND,
         NULL, WinEventProc, 0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS
     );
-
-    if (!hook) {
-        std::cerr << "Failed to install hook!" << std::endl;
-        return 1;
-    }
-
-    std::wcout << L"--- InputSchuyn is Running ---" << std::endl;
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
